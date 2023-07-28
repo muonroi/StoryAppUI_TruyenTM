d5827de76c12098027a7915ab163e0b5a70c80ab
muonroi <leanhphi1706@gmail.com>
2023-07-28 11:34:34 +0700

    Complete ux

---
+++ lib/Pages/MainPages/book_page.user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookOfUser extends StatefulWidget {
+  const BookOfUser({super.key});
+
+  @override
+  State<BookOfUser> createState() => _BookOfUserState();
+}
+
+class _BookOfUserState extends State<BookOfUser> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
